b5df8e33d2339c1bc63a2f5c8b38133615a06943
Rodrigo Gomez Palacio <11739227+rgomezp@users.noreply.github.com>
2025-08-04 15:23:59 -0500

    Update dependencies & ganon

--- apps/MyApp/app/services/ganon/ganon.ts
+++ apps/MyApp/app/services/ganon/ganon.ts
@@ -1,13 +1,14 @@
-import Ganon, { LogLevel } from "@rgomezp/ganon"
+import { Ganon, LogLevel } from "@rgomezp/ganon"
 import CLOUD_BACKUP_CONFIG from "./cloudConfig"
 import StorageMapping from "./StorageMapping"
 
 const logLevel = process.env.NODE_ENV === "development" ? LogLevel.VERBOSE : LogLevel.NONE
 
 // Initialize once using your specialized type.
-export const ganon: Ganon<StorageMapping> = Ganon.init<StorageMapping>({
+export const ganon: Ganon<StorageMapping> = new Ganon<StorageMapping>({
   identifierKey: "email",
   cloudConfig: CLOUD_BACKUP_CONFIG,
   logLevel,
-  instantBackup: true,
+  autoStartSync: false,
+  // remoteReadonly: true,
 })
