78af30fe85008e32ac7d248c53fc2ec7cad704f7
Rodrigo Gomez Palacio <11739227+rgomezp@users.noreply.github.com>
2025-08-04 19:16:48 -0500

    Subscribable

---
+++ apps/MyApp/app/scaffolding/Subscribable.ts
@@ -0,0 +1,29 @@
+import Log from "@/utils/Log"
+
+export default class Subscribable<MessageType> {
+  protected subscribers = new Set<(msg: MessageType) => void>()
+
+  /**
+   * Subscribe to the message stream.
+   * @param  { (msg:MessageType) => void } callback
+   */
+  public subscribe(callback: (msg: MessageType) => void): () => void {
+    this.subscribers.add(callback)
+    return () => this.subscribers.delete(callback)
+  }
+
+  /**
+   * Publish a message to the message stream.
+   * @param msg The message to publish
+   */
+  public broadcast(msg: MessageType) {
+    this.subscribers.forEach((callback) => {
+      callback(msg)
+    })
+  }
+
+  public clearSubscribers() {
+    Log.info("Subscribable: clearing subscribers")
+    this.subscribers.clear()
+  }
+}
