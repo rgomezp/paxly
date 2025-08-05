1c34ed1f89ac77fc36aca4c881d96b8d01b590b7
Rodrigo Gomez Palacio <11739227+rgomezp@users.noreply.github.com>
2025-08-05 14:42:43 -0500

    test: remove i18next mock from test setup
    - Remove i18next mock from jest setup configuration
    - Clean up test environment as part of i18next migration
    - Tests now use LanguageCopy system instead of i18next

--- apps/MyApp/test/setup.ts
+++ apps/MyApp/test/setup.ts
@@ -1,3 +1,5 @@
+/// <reference types="jest" />
+
 // we always make sure 'react-native' gets included first
 import * as ReactNative from "react-native"
 import mockFile from "./mockFile"
@@ -9,7 +11,7 @@ jest.doMock("react-native", () => {
     {
       Image: {
         ...ReactNative.Image,
-        resolveAssetSource: jest.fn((_source) => mockFile), // eslint-disable-line @typescript-eslint/no-unused-vars
+        resolveAssetSource: jest.fn((_source: any) => mockFile), // eslint-disable-line @typescript-eslint/no-unused-vars
         getSize: jest.fn(
           (
             uri: string, // eslint-disable-line @typescript-eslint/no-unused-vars
@@ -23,34 +25,7 @@ jest.doMock("react-native", () => {
   )
 })
 
-jest.mock("i18next", () => ({
-  currentLocale: "en",
-  t: (key: string, params: Record<string, string>) => {
-    return `${key} ${JSON.stringify(params)}`
-  },
-  translate: (key: string, params: Record<string, string>) => {
-    return `${key} ${JSON.stringify(params)}`
-  },
-}))
-
 jest.mock("expo-localization", () => ({
   ...jest.requireActual("expo-localization"),
   getLocales: () => [{ languageTag: "en-US", textDirection: "ltr" }],
 }))
-
-jest.mock("../app/i18n/i18n.ts", () => ({
-  i18n: {
-    isInitialized: true,
-    language: "en",
-    t: (key: string, params: Record<string, string>) => {
-      return `${key} ${JSON.stringify(params)}`
-    },
-    numberToCurrency: jest.fn(),
-  },
-}))
-
-declare const tron // eslint-disable-line @typescript-eslint/no-unused-vars
-
-declare global {
-  let __TEST__: boolean
-}
