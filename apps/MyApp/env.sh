6facf0c9ae26a192889825e843e82e3f76a1fbdb
Rodrigo Gomez Palacio <11739227+rgomezp@users.noreply.github.com>
2025-08-05 23:16:01 -0500

    Remove env.sh

--- apps/MyApp/env.sh
+++
@@ -1,2 +0,0 @@
-export GOOGLE_SERVICES_JSON=/Users/rodrigo/Projects/potion-forge/apps/MyApp/googleServices/google-services.json
-export GOOGLE_SERVICES_PLIST=/Users/rodrigo/Projects/potion-forge/apps/MyApp/googleServices/GoogleService-Info.plist
