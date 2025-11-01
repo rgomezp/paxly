de533e7442e14b3d0eda49f4977659bb6ce2664c
Rodrigo Gomez Palacio <11739227+rgomezp@users.noreply.github.com>
2025-10-31 19:08:10 -0500

    ios 3

--- apps/LetThemGo/app.config.ts
+++ apps/LetThemGo/app.config.ts
@@ -47,7 +47,7 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
       icon: "./assets/images/app-icon-ios.png",
       supportsTablet: true,
       bundleIdentifier: "com.honeywolf.letthemgo",
-      buildNumber: "2",
+      buildNumber: "3",
       config: {
         usesNonExemptEncryption: false,
       },
