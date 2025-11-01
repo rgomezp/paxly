302c7c1ba10ba47b840c6f1ea5e254723051583e
Rodrigo Gomez Palacio <11739227+rgomezp@users.noreply.github.com>
2025-11-01 02:38:49 -0500

    dead code

--- apps/LetThemGo/app.config.ts
+++ apps/LetThemGo/app.config.ts
@@ -36,7 +36,7 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
     android: {
       icon: "./assets/images/app-icon-android-legacy.png",
       package: "com.honeywolf.letthemgo",
-      versionCode: 1,
+      versionCode: 4,
       adaptiveIcon: {
         foregroundImage: "./assets/images/app-icon-android-adaptive-foreground.png",
         backgroundImage: "./assets/images/app-icon-android-adaptive-background.png",
--- apps/LetThemGo/app/entitlements/constants/referralThresholds.ts
+++
@@ -1,5 +0,0 @@
-import { FEATURES } from "./features"
-
-export const REFERRAL_COUNT_THRESHOLDS = {
-  [FEATURES.EXAMPLE_FEATURE]: 1,
-}
