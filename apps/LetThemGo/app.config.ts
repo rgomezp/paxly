cd7f2544130eef0ac53852399c531377856aec3d
Rodrigo Gomez Palacio <11739227+rgomezp@users.noreply.github.com>
2025-10-31 20:07:24 -0500

    Updates app configuration to TypeScript
    Migrates the app configuration file from JavaScript to TypeScript for improved type safety and maintainability.
    
    Also, resets the build number back to 0. This is likely to be a new major release due to the branch name "breakup-app".

--- apps/LetThemGo/app.config.ts
+++ apps/LetThemGo/app.config.ts
@@ -47,7 +47,7 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
       icon: "./assets/images/app-icon-ios.png",
       supportsTablet: true,
       bundleIdentifier: "com.honeywolf.letthemgo",
-      buildNumber: "4",
+      buildNumber: "0",
       config: {
         usesNonExemptEncryption: false,
       },
