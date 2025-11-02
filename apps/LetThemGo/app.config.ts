012ad629cc93c3ce7a6b3a1fa9a583292dc32f8e
Rodrigo Gomez Palacio <11739227+rgomezp@users.noreply.github.com>
2025-11-02 01:21:19 -0500

    Increments build versions
    Increments the Android versionCode and iOS buildNumber in preparation for a new release.
    This ensures users receive the latest updates on their devices.

--- apps/LetThemGo/app.config.ts
+++ apps/LetThemGo/app.config.ts
@@ -36,7 +36,7 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
     android: {
       icon: "./assets/images/app-icon-android-legacy.png",
       package: "com.honeywolf.letthemgo",
-      versionCode: 4,
+      versionCode: 5,
       adaptiveIcon: {
         foregroundImage: "./assets/images/app-icon-android-adaptive-foreground.png",
         backgroundImage: "./assets/images/app-icon-android-adaptive-background.png",
@@ -57,7 +57,7 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
       icon: "./assets/images/app-icon-ios.png",
       supportsTablet: true,
       bundleIdentifier: "com.honeywolf.letthemgo",
-      buildNumber: "4",
+      buildNumber: "5",
       config: {
         usesNonExemptEncryption: false,
       },
