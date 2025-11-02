04a62c367f34b30fd9b0b1041b358752ec5294d2
Rodrigo Gomez Palacio <11739227+rgomezp@users.noreply.github.com>
2025-11-01 23:21:57 -0500

    mascot name options

---
+++ apps/LetThemGo/app/types/MascotName.ts
@@ -0,0 +1,11 @@
+enum MascotName {
+  PLANTY = "planty",
+  WILLOW = "willow",
+  CHLOE = "chloe",
+  OLIVE = "olive",
+  FLORA = "flora",
+  PENNY = "penny",
+  SPROUT = "sprout",
+}
+
+export { MascotName as MascotNames }
