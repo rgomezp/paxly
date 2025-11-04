19e38fbc72cbff5f30683cb74aff439b4d7f2ea6
Rodrigo Gomez Palacio <11739227+rgomezp@users.noreply.github.com>
2025-11-03 20:50:31 -0600

    Add type definitions for new onboarding questions
    - Add YesNo enum type
    - Add NoContactReason enum type
    - Add ContactTemptationSituations enum type
    - Add AppMainGoal enum type

---
+++ apps/LetThemGo/app/types/ContactTemptationSituations.ts
@@ -0,0 +1,10 @@
+enum ContactTemptationSituations {
+  WHEN_LONELY = "when_lonely",
+  WHEN_SAD = "when_sad",
+  WHEN_SEEING_THEIR_POSTS = "when_seeing_their_posts",
+  ON_SPECIAL_DATES = "on_special_dates",
+  WHEN_DRUNK = "when_drunk",
+}
+
+export { ContactTemptationSituations as ContactTemptationSituationsChoices }
+
---
+++ apps/LetThemGo/app/types/YesNo.ts
@@ -0,0 +1,7 @@
+enum YesNo {
+  YES = "yes",
+  NO = "no",
+}
+
+export { YesNo as YesNoChoices }
+
