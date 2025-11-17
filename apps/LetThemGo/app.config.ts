a4bf09e7c6f2822bab2bcd9c2cf9d2e47f1c3df3
Rodrigo Gomez Palacio <11739227+rgomezp@users.noreply.github.com>
2025-11-17 15:59:38 -0600

    1.1.0 Release Commit

--- apps/LetThemGo/app.config.ts
+++ apps/LetThemGo/app.config.ts
@@ -39,7 +39,7 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
     slug: "let-them-go",
     scheme: "myapp",
     owner: "honeywolf",
-    version: "1.0.1",
+    version: "1.1.0",
     orientation: "portrait",
     userInterfaceStyle: "automatic",
     icon: "./assets/images/app-icon-all.png",
