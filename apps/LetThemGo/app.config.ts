df5f10016df27e28697349a64acf1c8cd12eed34
Rodrigo Gomez Palacio <11739227+rgomezp@users.noreply.github.com>
2025-11-17 23:44:06 -0600

    ios 17 build

--- apps/LetThemGo/app.config.ts
+++ apps/LetThemGo/app.config.ts
@@ -74,7 +74,7 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
       icon: "./assets/images/app-icon-ios.png",
       supportsTablet: true,
       bundleIdentifier: "com.honeywolf.letthemgo",
-      buildNumber: "16",
+      buildNumber: "17",
       config: {
         usesNonExemptEncryption: false,
       },
