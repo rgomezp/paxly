715cffc27781268ec0301292f4f23811789faa00
Rodrigo Gomez Palacio <11739227+rgomezp@users.noreply.github.com>
2026-01-20 17:40:15 -0600

    android 59

--- apps/AnxietyTracker/app.config.ts
+++ apps/AnxietyTracker/app.config.ts
@@ -58,7 +58,7 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
     android: {
       icon: "./assets/images/app-icon-android-legacy.png",
       package: "com.honeywolf.anxietytracker",
-      versionCode: 58,
+      versionCode: 59,
       adaptiveIcon: {
         foregroundImage: "./assets/images/app-icon-android-adaptive-foreground.png",
         backgroundImage: "./assets/images/app-icon-android-adaptive-background.png",
